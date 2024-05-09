bcd2d0260e4ef211986c28a7896b42ebb1a9359a
So Man Tik <1463647948qq@gmail.com>
2024-05-10 00:56:52 +0800

    add api function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup>
+import { ref, onMounted, computed, watch, nextTick } from 'vue'
+import { RouterLink,useRoute, useLink ,useRouter} from 'vue-router'
 
+const router = useRouter();
+const route = useRoute();
+
+var booking = ref('ff')
+
+
+const getBooking = async function () {
+
+const response = await fetch('/api'); 
+const json = await response.json();
+
+console.log(json);
+
+booking.value = json;
+}
+
+onMounted(async () => {
+
+        getBooking();
+});
 </script>
 
 <template>
   <main>
-   dfvfvfvfvdv
+   {{ booking }}
+   vvfvf
   </main>
 </template>
